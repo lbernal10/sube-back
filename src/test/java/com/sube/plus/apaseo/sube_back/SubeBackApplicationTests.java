4b0521f77dc3a18675fd43417080457c98041ec2
Francisco Bernal <contactolbernal@gmail.com>
2024-12-13 22:46:07 -0600

    Add files other repositories

--- src/test/java/com/sube/plus/apaseo/sube_back/SubeBackApplicationTests.java
+++ src/test/java/com/sube/plus/apaseo/sube_back/SubeBackApplicationTests.java
@@ -3,7 +3,7 @@ package com.sube.plus.apaseo.sube_back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.data.mongodb.uri=mongodb://localhost:27017/testdb"})
 class SubeBackApplicationTests {
 
 	@Test
