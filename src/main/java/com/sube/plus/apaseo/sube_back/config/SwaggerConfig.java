d1cf30e5c4662055687a4e0482c8188f154e5a43
Francisco Bernal <contactolbernal@gmail.com>
2024-12-15 01:07:27 -0600

    Add Security Login

--- src/main/java/com/sube/plus/apaseo/sube_back/config/SwaggerConfig.java
+++ src/main/java/com/sube/plus/apaseo/sube_back/config/SwaggerConfig.java
@@ -32,7 +32,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Sube Service API")
-                        .version("1.0.6")
+                        .version("1.0.7")
                         .description("Documentación de la API para Sube Service"));
     }
 }
