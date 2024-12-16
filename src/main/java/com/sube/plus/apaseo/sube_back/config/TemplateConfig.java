4da3eeae6ef2de41e607b2e1cd03312af5c4392a
Francisco Bernal <contactolbernal@gmail.com>
2024-12-16 00:05:40 -0600

    Send pwd by email fix

---
+++ src/main/java/com/sube/plus/apaseo/sube_back/config/TemplateConfig.java
@@ -0,0 +1,24 @@
+package com.sube.plus.apaseo.sube_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class TemplateConfig {
+
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setOrder(1);
+        resolver.setCheckExistence(true);
+
+        return resolver;
+    }
+}
