b2381dc7bde90ae0960ce46fc8f3ee3692f85781
Francisco Bernal <contactolbernal@gmail.com>
2024-12-15 18:20:24 -0600

    Send Verification Phone - twilio
    Send Verification Phone - twilio

--- src/main/java/com/sube/plus/apaseo/sube_back/util/SendPhone.java
+++ src/main/java/com/sube/plus/apaseo/sube_back/util/SendPhone.java
@@ -3,23 +3,36 @@ package com.sube.plus.apaseo.sube_back.util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class SendPhone {
 
-    //private final JavaMailSender mailSender;
+    @Value("${app.twilio.account.sid}")
+    private String appAccountSID;
 
-    public void sendVerificationPhone(String email, String verificationCode) {
-        /*
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Verification Code for Your Account");
-        message.setText("Your verification code is: " + verificationCode);
-        mailSender.send(message);
-        */
+    @Value("${app.twilio.account.token}")
+    private String appAccountToken;
+
+    @Value("${app.twilio.account.phone}")
+    private String appAccountPhone;
+
+    public void sendVerificationPhone(String phone, String verificationCode) {
+        Twilio.init(appAccountSID, appAccountToken);
+
+        Message message = Message
+                .creator(new com.twilio.type.PhoneNumber(phone),
+                        new com.twilio.type.PhoneNumber(appAccountPhone),
+                        "(Sube+) Su código de verificación es: " + verificationCode)
+                .create();
+
+
+        System.out.println(message.getBody());
         log.info("Send successfully code for your phone");
     }
 
